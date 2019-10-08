d9c60f3644b44f6f99de886f92e0c9af3cdb52d6
sakda homhuan <sakda.homhuan@gmail.com>
2019-10-08 21:19:24 +0700

    delete firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,3 @@
 export const environment = {
-  production: true,
-  firebaseConfig: {
-    apiKey: "AIzaSyAzqg4t9unxySz869aDBotGyxfQ9H1xG98",
-    authDomain: "mpd-drone.firebaseapp.com",
-    databaseURL: "https://mpd-drone.firebaseio.com",
-    projectId: "mpd-drone",
-    storageBucket: "mpd-drone.appspot.com",
-    messagingSenderId: "134447419137",
-    appId: "1:134447419137:web:9c1f9b0f4381411a2ed500"
-  }
+  production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,16 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  firebaseConfig: {
-    apiKey: "AIzaSyAzqg4t9unxySz869aDBotGyxfQ9H1xG98",
-    authDomain: "mpd-drone.firebaseapp.com",
-    databaseURL: "https://mpd-drone.firebaseio.com",
-    projectId: "mpd-drone",
-    storageBucket: "mpd-drone.appspot.com",
-    messagingSenderId: "134447419137",
-    appId: "1:134447419137:web:9c1f9b0f4381411a2ed500"
-  }
+  production: false
 };
 
 /*
